a054eb0c779304d3c3204e443a296cecd8f859cc
Jianzu Guan <jianzuguan@hotmail.com>
2021-03-14 20:20:40 +0000

    add algorithms control params

--- src/PathFindingVisualiser/index.tsx
+++ src/PathFindingVisualiser/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Button, TextField } from "@material-ui/core";
 import TypeNode from "../types/Node";
 import Components from "../components";
 import dijkstra, { getNodesInShortestPathOrder } from "../algorithms/dijkstra";
@@ -16,12 +17,19 @@ const NOISE_ZOOM = 0.25;
 const NOISE_OFFSET_ROW = 100;
 const NOISE_OFFSET_COL = 10;
 
-const createNode = (col: number, row: number): TypeNode => {
+const createNode = (
+  col: number,
+  row: number,
+  startNodeRow: number,
+  startNodeCol: number,
+  endNodeRow: number,
+  endNodeCol: number
+): TypeNode => {
   return {
     col,
     row,
-    isStart: START_NODE_COL === col && START_NODE_ROW === row,
-    isEnd: END_NODE_COL === col && END_NODE_ROW === row,
+    isStart: startNodeCol === col && startNodeRow === row,
+    isEnd: endNodeCol === col && endNodeRow === row,
     weight: noise(
       col * NOISE_ZOOM + NOISE_OFFSET_COL,
       row * NOISE_ZOOM + NOISE_OFFSET_ROW
@@ -32,12 +40,21 @@ const createNode = (col: number, row: number): TypeNode => {
   };
 };
 
-const getInitialGrid = () => {
+const getInitialGrid = (
+  numRows = NUM_ROWS,
+  numCols = NUM_COLS,
+  startNodeRow = START_NODE_ROW,
+  startNodeCol = START_NODE_COL,
+  endNodeRow = END_NODE_ROW,
+  endNodeCol = END_NODE_COL
+) => {
   const grid: TypeNode[][] = [];
-  for (let row = 0; row < NUM_ROWS; row++) {
+  for (let row = 0; row < numRows; row++) {
     const currentRow = [];
-    for (let col = 0; col < NUM_COLS; col++) {
-      currentRow.push(createNode(col, row));
+    for (let col = 0; col < numCols; col++) {
+      currentRow.push(
+        createNode(col, row, startNodeRow, startNodeCol, endNodeRow, endNodeCol)
+      );
     }
     grid.push(currentRow);
   }
@@ -45,6 +62,13 @@ const getInitialGrid = () => {
 };
 
 const PathFindingVisualiser = () => {
+  const [numRows, setNumRows] = useState(NUM_ROWS);
+  const [numCols, setNumCols] = useState(NUM_COLS);
+  const [startNodeRow, setStartNodeRow] = useState(START_NODE_ROW);
+  const [startNodeCol, setStartNodeCol] = useState(START_NODE_COL);
+  const [endNodeRow, setEndNodeRow] = useState(END_NODE_ROW);
+  const [endNodeCol, setEndNodeCol] = useState(END_NODE_COL);
+
   const [grid, setGrid] = useState<TypeNode[][]>();
 
   const animateShortestPath = (nodesInShortestPathOrder: TypeNode[]) => {
@@ -53,8 +77,8 @@ const PathFindingVisualiser = () => {
         const node = nodesInShortestPathOrder[i];
         if (
           node === null ||
-          (START_NODE_ROW === node.row && START_NODE_COL === node.col) ||
-          (END_NODE_ROW === node.row && END_NODE_COL === node.col)
+          (startNodeRow === node.row && startNodeCol === node.col) ||
+          (endNodeRow === node.row && endNodeCol === node.col)
         ) {
           return;
         }
@@ -84,8 +108,8 @@ const PathFindingVisualiser = () => {
         const node = visitedNodesInOrder[i];
         if (
           node === null ||
-          (START_NODE_ROW === node.row && START_NODE_COL === node.col) ||
-          (END_NODE_ROW === node.row && END_NODE_COL === node.col)
+          (startNodeRow === node.row && startNodeCol === node.col) ||
+          (endNodeRow === node.row && endNodeCol === node.col)
         ) {
           return;
         }
@@ -101,8 +125,8 @@ const PathFindingVisualiser = () => {
 
   const visualiseDijkstra = () => {
     if (grid === undefined) return;
-    const startNode = grid[START_NODE_ROW][START_NODE_COL];
-    const endNode = grid[END_NODE_ROW][END_NODE_COL];
+    const startNode = grid[startNodeRow][startNodeCol];
+    const endNode = grid[endNodeRow][endNodeCol];
     const visitedNodesInOrder = dijkstra(grid, startNode, endNode);
     console.log(visitedNodesInOrder);
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(endNode);
@@ -113,10 +137,79 @@ const PathFindingVisualiser = () => {
     const initialGrid = getInitialGrid();
     setGrid(initialGrid);
   }, []);
+
+  useEffect(() => {
+    const initialGrid = getInitialGrid(
+      numRows,
+      numCols,
+      startNodeRow,
+      startNodeCol,
+      endNodeRow,
+      endNodeCol
+    );
+    setGrid(initialGrid);
+  }, [numRows, numCols, startNodeRow, startNodeCol, endNodeRow, endNodeCol]);
+
   return (
     <>
-      <div>
-        <button onClick={visualiseDijkstra}>Just do it</button>
+      <div className="side-bar">
+        <Button variant="contained" color="primary" onClick={visualiseDijkstra}>
+          Just do it
+        </Button>
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="number-of-rows-text-field"
+          label="Number of Rows"
+          value={numRows}
+          onChange={(e) => setNumRows(Number(e.target.value) || NUM_ROWS)}
+        />
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="number-of-cols-text-field"
+          label="Number of col"
+          value={numCols}
+          onChange={(e) => setNumCols(Number(e.target.value) || NUM_COLS)}
+        />
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="start-node-row-text-field"
+          label="Start Node Row"
+          value={startNodeRow}
+          onChange={(e) => setStartNodeRow(Number(e.target.value) || 1)}
+        />
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="start-node-col-text-field"
+          label="Start Node Col"
+          value={startNodeCol}
+          onChange={(e) => setStartNodeCol(Number(e.target.value) || 1)}
+        />
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="end-node-row-text-field"
+          label="End Node Row"
+          value={endNodeRow}
+          onChange={(e) => setEndNodeRow(Number(e.target.value) || 1)}
+        />
+
+        <TextField
+          variant="outlined"
+          type="number"
+          id="end-node-col-text-field"
+          label="End Node Col"
+          value={endNodeCol}
+          onChange={(e) => setEndNodeCol(Number(e.target.value) || 1)}
+        />
       </div>
       <div className="grid">
         {grid?.map((row, rowIndex) => {
