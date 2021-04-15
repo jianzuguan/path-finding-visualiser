fee68e560b3b59395b4f366c882e20472533a642
Jianzu Guan <jianzuguan@hotmail.com>
2021-04-15 17:33:34 +0100

    add step by step dijkstra

--- src/algorithms/dijkstra.ts
+++ src/algorithms/dijkstra.ts
@@ -10,6 +10,18 @@ const getAllNodes = (grid: TypeNode[][]) => {
   return nodes;
 };
 
+const getAllUnvisitedNode = (grid: TypeNode[][]) => {
+  const unvisitedNodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      if (!node.isVisited) {
+        unvisitedNodes.push(node);
+      }
+    }
+  }
+  return unvisitedNodes;
+};
+
 const sortNodesByDistance = (unvisitedNoes: TypeNode[]) => {
   unvisitedNoes.sort(
     (nodeA: TypeNode, nodeB: TypeNode) => nodeA.distance - nodeB.distance
@@ -42,11 +54,11 @@ const updateUnvisitedNeighbors = (
 
 const dijkstra = (
   grid: TypeNode[][],
-  stratNode: TypeNode,
+  startNode: TypeNode,
   finishNode: TypeNode
 ) => {
   const unvisitedNodes = getAllNodes(grid);
-  stratNode.distance = 0;
+  startNode.distance = 0;
   const visitedNodesInOrder = [];
   while (!!unvisitedNodes.length) {
     sortNodesByDistance(unvisitedNodes);
@@ -72,6 +84,60 @@ export const getNodesInShortestPathOrder = (endNode: TypeNode) => {
     currentNode = currentNode.previousNode;
   }
   return nodesInShortestPathOrder;
+};
+
+export const init = (grid: TypeNode[][], startNode: TypeNode): TypeNode[][] => {
+  const gridClone = [...grid].map((row) => [...row]);
+
+  if (
+    gridClone.length > startNode.y &&
+    gridClone[startNode.y].length > startNode.x
+  ) {
+    gridClone[startNode.y][startNode.x].distance = 0;
+  }
+
+  return gridClone;
+};
+
+export const hasVisitedFinishNode = (
+  grid: TypeNode[][],
+  finishNode: TypeNode
+) => {
+  if (finishNode.x < 0 || finishNode.y < 0) {
+    return false;
+  }
+
+  if (grid.length > finishNode.y && grid[finishNode.y].length > finishNode.x) {
+    return grid[finishNode.y][finishNode.x].isVisited;
+  }
+
+  return false;
+};
+
+export const hasNext = (grid: TypeNode[][], finishNode: TypeNode) => {
+  const unvisitedNodes = getAllUnvisitedNode(grid);
+
+  sortNodesByDistance(unvisitedNodes);
+  const closestNode = unvisitedNodes.shift();
+  // All nodes in grid visited.
+  if (!closestNode) return false;
+  // All reachable nodes visited.
+  if (closestNode.distance === Infinity) return false;
+
+  return true;
+};
+
+export const next = (grid: TypeNode[][]) => {
+  const gridClone = [...grid].map((row) => [...row]);
+
+  const unvisitedNodes = getAllUnvisitedNode(gridClone);
+  sortNodesByDistance(unvisitedNodes);
+  const closestNode = unvisitedNodes.shift();
+  if (!closestNode) return gridClone;
+  closestNode.isVisited = true;
+  updateUnvisitedNeighbors(closestNode, gridClone);
+
+  return gridClone;
 };
 
 export default dijkstra;
